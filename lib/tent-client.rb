302b3436a57ce43c964596d741860bca858a419f
Jesse Stuart <jesse@jessestuart.ca>
2013-03-27 17:07:51 -0400

    Base class initializer

--- lib/tent-client.rb
+++ lib/tent-client.rb
@@ -1,6 +1,16 @@
 require 'tent-client/version'
+require 'tent-client/discovery'
 
 class TentClient
-  def initialize
+
+  attr_reader :entity_uri
+  def initialize(entity_uri, options = {})
+    @server_meta = options.delete(:server_meta)
+    @entity_uri, @options = entity_uri, options
   end
+
+  def server_meta
+    @server_meta ||= Discovery.discover(entity_uri)
+  end
+
 end
