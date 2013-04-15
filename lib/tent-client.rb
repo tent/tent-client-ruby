a1f051e778bc10b780b26dfd132e77b47738baee
Jesse Stuart <jesse@jessestuart.ca>
2013-04-15 16:34:50 -0400

    Add authorization header when credentials provided

--- lib/tent-client.rb
+++ lib/tent-client.rb
@@ -4,6 +4,7 @@ require 'tent-client/multipart-post/parts'
 require 'tent-client/tent_type'
 require 'tent-client/middleware/content_type_header'
 require 'tent-client/middleware/encode_json'
+require 'tent-client/middleware/authentication'
 require 'tent-client/cycle_http'
 require 'tent-client/discovery'
 require 'tent-client/post'
@@ -29,6 +30,7 @@ class TentClient
     CycleHTTP.new(self) do |f|
       f.use Middleware::ContentTypeHeader
       f.use Middleware::EncodeJson unless @options[:skip_serialization]
+      f.use Middleware::Authentication, @options[:credentials] if @options[:credentials]
       f.response :multi_json, :content_type => /\bjson\Z/ unless @options[:skip_serialization]
       f.adapter *Array(faraday_adapter)
     end
---
+++ lib/tent-client/middleware/authentication.rb
@@ -0,0 +1,69 @@
+require 'faraday_middleware'
+
+class TentClient
+  module Middleware
+    class Authentication < Faraday::Middleware
+      AUTHORIZATION_HEADER = 'Authorization'.freeze
+      CONTENT_TYPE_HEADER = 'Content-Type'.freeze
+
+      def initialize(app, credentials)
+        super(app)
+        @credentials = {
+          :id => credentials[:id],
+          :key => credentials[:hawk_key],
+          :algorithm => credentials[:hawk_algorithm]
+        } if credentials
+      end
+
+      def call(env)
+        env[:request_headers][AUTHORIZATION_HEADER] = build_authorization_header(env) if @credentials
+        @app.call env
+      end
+
+      private
+
+      def build_authorization_header(env)
+        Hawk::Client.build_authorization_header(
+          :credentials => @credentials,
+          :ts => Time.now.to_i,
+          :payload => request_body(env),
+          :content_type => request_content_type(env),
+          :method => request_method(env),
+          :port => request_port(env),
+          :host => request_host(env),
+          :path => request_path(env)
+        )
+      end
+
+      def request_body(env)
+        if env[:body].respond_to?(:read)
+          body = env[:body].read
+          env[:body].rewind if env[:body].respond_to?(:rewind)
+          body
+        else
+          env[:body]
+        end
+      end
+
+      def request_content_type(env)
+        env[:request_headers]['Content-Type'].to_s.split(';').first
+      end
+
+      def request_method(env)
+        env[:method].to_s.upcase
+      end
+
+      def request_port(env)
+        env[:url].port || (env[:url].scheme == 'https' ? 443 : 80)
+      end
+
+      def request_host(env)
+        env[:url].host
+      end
+
+      def request_path(env)
+        env[:url].to_s.sub(%r{\A#{env[:url].scheme}://#{env[:url].host}}, '') # maintain query and fragment
+      end
+    end
+  end
+end
