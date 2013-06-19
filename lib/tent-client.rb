055d2890c140e72eeacefea232b7d47c4c9ca659
Jesse Stuart <jesse@jessestuart.ca>
2013-06-19 14:57:34 -0400

    Allow skipping response serialization while serializing request body

--- lib/tent-client.rb
+++ lib/tent-client.rb
@@ -60,7 +60,7 @@ class TentClient
       f.use Middleware::ContentTypeHeader
       f.use Middleware::EncodeJson unless @options[:skip_serialization]
       f.use Middleware::Authentication, @options[:credentials] if @options[:credentials]
-      f.response :multi_json, :content_type => /\bjson\Z/ unless @options[:skip_serialization]
+      f.response :multi_json, :content_type => /\bjson\Z/ unless @options[:skip_serialization] || @options[:skip_response_serialization]
       f.adapter *Array(faraday_adapter)
     end
   end
