10861d80e201870bc93169380ab10ad21feec31c
Jesse Stuart <jesse@jessestuart.ca>
2013-06-20 13:58:08 -0400

    Allow importing posts with attachments

--- lib/tent-client/middleware/content_type_header.rb
+++ lib/tent-client/middleware/content_type_header.rb
@@ -9,12 +9,12 @@ class TentClient
       def call(env)
         if env[:body] && Hash === env[:body] && !env[:request_headers][CONTENT_TYPE_HEADER]
           env[:request_headers][CONTENT_TYPE_HEADER] = MEDIA_TYPE % (env[:body]['type'] || env[:body][:type])
+        end
 
-          if env[:request]['tent.import']
-            env[:request_headers][CONTENT_TYPE_HEADER] << %(; rel="https://tent.io/rels/import")
-          elsif env[:request]['tent.notification']
-            env[:request_headers][CONTENT_TYPE_HEADER] << %(; rel="https://tent.io/rels/notification")
-          end
+        if env[:request]['tent.import']
+          env[:request_headers][CONTENT_TYPE_HEADER] << %(; rel="https://tent.io/rels/import")
+        elsif env[:request]['tent.notification']
+          env[:request_headers][CONTENT_TYPE_HEADER] << %(; rel="https://tent.io/rels/notification")
         end
 
         @app.call env
